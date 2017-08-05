dc473854cfab8bfe5c7c6a554d96470e4004e959
Steven Yang <askysy@hotmail.com>
2017-08-05 17:41:52 +0800

    add example

--- example.cpp
+++ example.cpp
@@ -1,12 +1,15 @@
 #include "BloomFilter.h"
-
+#include <iostream>
 
 int main() {
 	std::string a = "asdasdasdasdasdasdad";
 	std::string b = "asdasdbsdasdasdasdad";
 	BloomFilter fillter = BloomFilter(380, 8);
 	fillter.Add(a);
-	bool is_contain = fillter.Test(b);
+	bool is_contain = false;
+	is_contain = fillter.Test(b);
+	std::cout << "is_contain = " << is_contain << std::endl;
 	is_contain = fillter.Test(a);
+	std::cout << "is_contain = " << is_contain << std::endl;
 	return 0;
 }
